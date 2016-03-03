76875f27c775379e0642796b6c5ce389578a7072
Raksit <raksit.m@ku.th>
2016-03-03 18:11:41 +0700

    Added synonym

--- app/src/main/java/com/example/softspec/minidictionary/models/Word.java
+++ app/src/main/java/com/example/softspec/minidictionary/models/Word.java
@@ -1,7 +1,9 @@
 package com.example.softspec.minidictionary.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Earth, Boss, Nott on 29/2/2559.
@@ -10,15 +12,29 @@ public class Word implements Serializable {
 
     private String title;
     private String meaning;
+    private List<Word> synonym;
 
     public Word(String title, String meaning){
         this.title = title;
         this.meaning = meaning;
+        synonym = new ArrayList<Word>();
     }
 
-    public String getTitle(){return title;}
+    public String getTitle(){
+        return title;
+    }
 
-    public String getMeaning() {return meaning;}
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public List<Word> getSynonym() {
+        return synonym;
+    }
+
+    public void addSynonym(Word word) {
+        synonym.add(word);
+    }
 
     @Override
     public boolean equals(Object o) {
